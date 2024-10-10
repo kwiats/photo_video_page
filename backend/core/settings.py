8c8626895c9c36383a4358447c4c02c96c8c9062
kwiats <pawuskwiatkowski@gmail.com>
2024-10-10 20:56:19 +0200

    poprawki dla settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,10 +14,6 @@ import os
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv('envs/django.env')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
